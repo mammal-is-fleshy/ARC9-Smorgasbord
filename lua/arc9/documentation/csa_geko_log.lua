ab2b7c8160045679e6bafaf4246d93cb39511a9c
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2022-07-01 15:42:40 +0700

    i have a walky nose help

--- lua/arc9/documentation/csa_geko_log.lua
+++ lua/arc9/documentation/csa_geko_log.lua
@@ -6,6 +6,22 @@ Contre Srike Sorse in 2022 Gmod Tacticle Reelesm :tm:
 
 
 Google Images - ref
+Arctic - Incredibly doggy original restoration models
+Kali - Some M4 Parts (  Dragoon Guard, 607,608 Stock/Guard, Swire Stock  )
+CSGO, CSCD - Sounds??
+https://gamebanana.com/mods/210646 - SPR Guard
+https://gamebanana.com/mods/210602 - Targeter Guard
+Soldier of Fortune 3 - Muzzles and Tac/Flashlight
+
+https://gamebanana.com/models/3241 - T1
+https://gamebanana.com/skins/162976 - MRS 
+https://gamebanana.com/models/4134 - OKP7
+-- the attachment models are probably from contract wars
+
+https://gamebanana.com/mods/39876 - Muzzle/Grip/T1
+
+
+
 
 -------------
 [checklist]
@@ -13,14 +29,13 @@ Google Images - ref
 AWP
 M4
 AK
-M9
+M9???
 Glock???
-MP5/UMP
+MP5/UMP/G3
 Mac10??
 M3
 Makarov??
-[pretty much <2 per category]
-
+FAL???????????
 
 
 --- Basicall Dev Log -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -34,6 +49,13 @@ FAL: Model Quarter
 MP5: Model Quarter
 
 
+
+
+
+
+
+
+
 AK:
 Woodsman Handguard
 "Longest Boi"
@@ -43,12 +65,6 @@ Oppressor Handguard
 "The hell? It's just slightly longer"
 Stalker Handguard
 "Mate just use a silencer"
-
-
-
-
-
-
 
 
 
