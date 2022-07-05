3c4b0f259b7d031995238e1450318a81ca956ba8
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2022-07-05 19:52:45 +0700

    range

--- lua/arc9/common/attachments/gekolt_css_optic_kemperxl.lua
+++ lua/arc9/common/attachments/gekolt_css_optic_kemperxl.lua
@@ -29,3 +29,50 @@ ATT.ModelOffset = Vector(0, 0, -0.1)
 ATT.SwayAdd = 0.02
 ATT.FreeAimRadiusMultSights = 0.25
 ATT.SprintToFireTimeAdd = 0.035
+
+if CLIENT then
+    surface.CreateFont("css_font_digi", {
+        font = "Arial",
+        size = 25,
+        weight = 625,
+        antialias = true,
+        extended = true
+    })
+end
+
+if CLIENT then
+
+    ATT.HoloSightFunc = function(swep, pos, mdl)
+        local col = Color(200, 200, 250, 150)
+        local col_tp = Color(col.r, col.g, col.b, 1)
+        local ang = mdl:GetAngles()
+        ang:RotateAroundAxis(ang:Right(), 90)
+        ang:RotateAroundAxis(ang:Up(), -90)
+        cam.Start3D2D(pos - (ang:Right() * 800) - (ang:Forward() * 1024), ang, 8)
+        surface.SetDrawColor(col_tp)
+        surface.SetDrawColor(col)
+
+        local top = "-"
+        local d = 32000
+
+        local tr = util.TraceLine({
+            start = mdl:GetPos(),
+            endpos = mdl:GetPos() + (mdl:GetAngles():Forward() * d),
+            mask = MASK_SHOT,
+            filter = swep:GetOwner()
+        })
+
+        if tr.HitSky then
+            top = "NO TARGET"
+        else
+            top = tostring(math.ceil(tr.Fraction * d * ARC9.HUToM)) .. "m"
+        end
+
+        surface.SetTextColor(col)
+        surface.SetFont("css_font_digi")
+        surface.SetTextPos(128 - (surface.GetTextSize(top) / 2), -5)
+        surface.DrawText(top)
+
+        cam.End3D2D()
+    end
+end
