a81b08eb1708949afa5023476ba7d1de2904cd35
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2022-07-09 20:57:17 +0700

    fessugg does not exist

---
+++ lua/arc9/common/attachments/gekolt_css_optic_holo2.lua
@@ -0,0 +1,64 @@
+ATT.PrintName = "Holo Prototype Sight"
+ATT.CompactName = "H.Proto"
+ATT.Icon = Material("")
+ATT.Description = [[]]
+ATT.SortOrder = 0.2
+
+ATT.Model = "models/weapons/geckololt_css/atts/holo2.mdl"
+
+ATT.Category = {"optic_css","optic_css_m"}
+ATT.Folder = "REFLEX"
+
+// Allows a custom sight position to be defined
+ATT.Sights = {
+    {
+        Pos = Vector(-0.015, 6, -1.07),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.3,
+        ViewModelFOV = 60
+    }
+}
+
+ATT.ModelOffset = Vector(0, 0, -0.1)
+
+ATT.SwayAdd = 0.02
+ATT.FreeAimRadiusMultSights = 0.25
+ATT.SprintToFireTimeAdd = 0.035
+
+ATT.RTScope = true
+ATT.RTScopeSubmatIndex = 4
+ATT.RTScopeFOV = 10
+ATT.RTScopeRes = 1024
+ATT.RTScopeReticle = Material("arc9_css/holo.png", "mips smooth")
+ATT.RTScopeReticleScale = 0.75
+ATT.RTScopeColorable = true
+ATT.RTScopeNoPP = true
+ATT.RTScopeNoShadow = true
+
+ATT.RTScopeFLIR = true
+ATT.RTScopeFLIRHighlightColor = Color(255, 30, 30)
+ATT.RTScopeFLIRMonochrome = true
+ATT.RTScopeFLIRNoPP = true
+ATT.RTScopeFLIRBlend = 0.075
+ATT.RTScopeFLIRCCHot = { -- Color correction drawn only on FLIR targets
+    ["$pp_colour_addr"] = 0,
+    ["$pp_colour_addg"] = 0,
+    ["$pp_colour_addb"] = 0,
+    ["$pp_colour_brightness"] = 0.005,
+    ["$pp_colour_contrast"] = 1.5,
+    ["$pp_colour_colour"] = 1,
+    ["$pp_colour_mulr"] = 0,
+    ["$pp_colour_mulg"] = 0,
+    ["$pp_colour_mulb"] = 0
+}
+ATT.RTScopeFLIRCCCold = { -- Color correction drawn only on FLIR targets
+    ["$pp_colour_addr"] = 0,
+    ["$pp_colour_addg"] = 0,
+    ["$pp_colour_addb"] = 0,
+    ["$pp_colour_brightness"] = 0,
+    ["$pp_colour_contrast"] = 0.75,
+    ["$pp_colour_colour"] = 1,
+    ["$pp_colour_mulr"] = 0,
+    ["$pp_colour_mulg"] = 0,
+    ["$pp_colour_mulb"] = 0
+}
--- lua/arc9/common/attachments/gekolt_css_optic_micro.lua
+++ lua/arc9/common/attachments/gekolt_css_optic_micro.lua
@@ -4,7 +4,7 @@ ATT.Icon = Material("")
 ATT.Description = [[2x Magnifying Scope]]
 ATT.SortOrder = 4
 
-ATT.Model = "models/weapons/geckololt_css/atts/micro.mdl"
+ATT.Model = "models/weapons/geckololt_css/atts/micro2.mdl"
 
 ATT.Category = "optic_css"
 ATT.Folder = "SCOPE"
@@ -12,7 +12,7 @@ ATT.Folder = "SCOPE"
 // Allows a custom sight position to be defined
 ATT.Sights = {
     {
-        Pos = Vector(0.005, 9, -1.87),
+        Pos = Vector(0.005, 8.25, -1.87),
         Ang = Angle(0, 0, 0),
         Magnification = 1.5,
         ViewModelFOV = 40
@@ -20,7 +20,7 @@ ATT.Sights = {
 }
 
 ATT.RTScope = true
-ATT.RTScopeSubmatIndex = 2
+ATT.RTScopeSubmatIndex = 3
 ATT.RTScopeFOV = 10
 ATT.RTScopeReticle = Material("arc9_css/scope2_border.png", "mips smooth")
 ATT.RTScopeReticleScale = 0.8
@@ -30,9 +30,31 @@ ATT.RTScopeShadowIntensity = 7
 ATT.ScopeScreenRatio = 0.9
 
 ATT.Scale = 1.25
-ATT.ModelOffset = Vector(0, 0, -0.25)
+ATT.ModelOffset = Vector(-1, 0, -0.25)
 
 ATT.SwayAdd = 0.05
 ATT.FreeAimRadiusMultSights = 0.1
 ATT.AimDownSightsTimeAdd = 0.12
 ATT.SprintToFireTimeAdd = 0.05
+
+
+ATT.Attachments = {
+    {
+        PrintName = "MOUNT TOP",
+        Category = {"tac_css", "mount_css", "optic_css_s"},
+        Pos = Vector(-1,0, -2.7),
+        Ang = Angle(0, 0, 0),
+    },		
+    {
+        PrintName = "MOUNT LEFT",
+        Category = {"mountr_css", "tac_css"},
+        Pos = Vector(-1, -1.25, -1.5),
+        Ang = Angle(180, 180, 90),			
+    },
+    {
+        PrintName = "MOUNT Right",
+        Category = {"mountl_css", "tac_css"},
+        Pos = Vector(-1,1.25, -1.5),
+        Ang = Angle(0, 0, 90),
+    },	
+}
