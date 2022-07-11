8204cd3d88b8dc7f5dfe010466156ec90118e145
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2022-07-11 21:49:42 +0700

    mrs reticle change

--- lua/arc9/common/attachments/gekolt_css_optic_mrs.lua
+++ lua/arc9/common/attachments/gekolt_css_optic_mrs.lua
@@ -21,7 +21,7 @@ ATT.Sights = {
 
 ATT.HoloSight = true
 ATT.HoloSightReticle = Material("arc9_css/mrs.png", "mips smooth")
-ATT.HoloSightSize = 900
+ATT.HoloSightSize = 1200
 ATT.HoloSightColorable = true
 
 ATT.ModelOffset = Vector(0, 0, -0.1)
