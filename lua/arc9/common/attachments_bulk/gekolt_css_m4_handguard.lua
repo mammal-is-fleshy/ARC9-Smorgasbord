11892e799acfc2eaed63fcdd089b7e07c5f1230a
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2023-01-07 03:07:44 +0700

    new render for m4 att

--- lua/arc9/common/attachments_bulk/gekolt_css_m4_handguard.lua
+++ lua/arc9/common/attachments_bulk/gekolt_css_m4_handguard.lua
@@ -6,7 +6,7 @@ ATT.PrintName = "Naval Handguard"
 ATT.CompactName = "Naval"
 ATT.Description = [[Take a deep dive in the island hopping campaign of the Pacific.]]
 
-ATT.Icon = Material("entities/gekolt_css_m4_h_dragoon.png", "mips smooth")
+ATT.Icon = Material("entities/gekolt_css_m4_h_t86.png", "mips smooth")
 
 ATT.SortOrder = 2.8
 ATT.Category = "css_m4_handguard"
