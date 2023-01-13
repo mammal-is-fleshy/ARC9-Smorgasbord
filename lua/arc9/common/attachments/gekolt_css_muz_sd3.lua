ad583a4634b8e672037663201eb44a4c7cf9f231
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2023-01-13 16:19:15 +0700

    suppressor 3

---
+++ lua/arc9/common/attachments/gekolt_css_muz_sd3.lua
@@ -0,0 +1,27 @@
+ATT.PrintName = "Suppressor C"
+ATT.CompactName = "SUPP C"
+ATT.Icon = Material("entities/gekolt_css_muz_sd2.png", "mips smooth")
+ATT.Description = [[]]
+ATT.SortOrder = 1
+
+ATT.Model = "models/weapons/geckololt_css/atts/sd_m3.mdl"
+ATT.Category = "muzzle_css"
+
+ATT.Scale = 1.25
+ATT.ModelOffset = Vector(0.25, 0, 0)
+
+ATT.PhysBulletMuzzleVelocityMult = 0.8
+ATT.RangeMaxMult = 1.2
+ATT.RangeMinMult = 0.8
+
+ATT.AimDownSightsTimeMult = 1.1
+ATT.SprintToFireTimeMult = 1.08
+
+ATT.Silencer = true
+ATT.ShootVolumeMult = 0.75
+ATT.MuzzleParticleOverride = "muzzleflash_suppressed"
+ATT.MuzzleParticleOverride_Priority = 10
+ATT.MuzzleDevice = true
+
+ATT.RecoilUpMult = 1.05
+ATT.RecoilRandomMult = 0.8
