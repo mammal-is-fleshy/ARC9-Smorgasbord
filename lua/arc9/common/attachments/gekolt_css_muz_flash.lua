b6dd72831290d87b290657f7c9d9fde652a98ba7
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2023-02-03 11:27:14 +0700

    kojimba

--- lua/arc9/common/attachments/gekolt_css_muz_flash.lua
+++ lua/arc9/common/attachments/gekolt_css_muz_flash.lua
@@ -6,11 +6,11 @@ ATT.Icon = Material("entities/gekolt_css_muz_flash.png", "mips smooth")
 ATT.SortOrder = 1
 
 ATT.Model = "models/weapons/geckololt_css/atts/muz_cage.mdl"
-ATT.ModelBodygroups = "1"
+ATT.ModelBodygroups = "2"
 ATT.Category = "muzzle_css"
 
 ATT.Scale = 1.25
-ATT.ModelOffset = Vector(0.25, 0, -0.01)
+ATT.ModelOffset = Vector(0.25, 0, -0.075)
 
 ATT.ShootVolumeMult = 1.1
 ATT.MuzzleParticleOverride = "muzzleflash_4"
